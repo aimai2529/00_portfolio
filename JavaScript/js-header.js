5b34ef8b7c05c3c0a622a2fae4bc865f70ce05a6
aimai <it242229@trident.ac.jp>
2025-04-08 16:03:51 +0900

    トップページ完成

--- JavaScript/js-header.js
+++ JavaScript/js-header.js
@@ -1,10 +1,49 @@
 const header = document.querySelector(".js-header");
+const btn = document.querySelector(".js-btn");
+const bodyHeight = document.body.clientHeight // bodyの高さを取得
+const windowHeight = window.innerHeight // windowの高さを取得
+const bottomPoint = bodyHeight - windowHeight // ページ最下部までスクロールしたかを判定するための位置を計算
+
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 150 && header.classList.contains("is-active")) {
         header.classList.remove("is-active");
-        console.log("Header deactivated");
+        btn.classList.add("is-close");
     } else if (window.scrollY < 150 && !header.classList.contains("is-active")) {
+        header.classList.remove("is-appear");
         header.classList.add("is-active");
+        btn.classList.remove("is-close");
+        btn.classList.remove("is-open");
+    }
+
+    if (window.scrollY >= bottomPoint && !header.classList.contains("is-bottom")) {
+        header.classList.remove("is-appear");
+        header.classList.add("is-bottom");
+        btn.classList.remove("is-close");
+        btn.classList.remove("is-open");
+    } else if (window.scrollY < bottomPoint && header.classList.contains("is-bottom")) {
+        header.classList.remove("is-bottom");
+        btn.classList.add("is-close");
     }
 });
+
+// header.addEventListener('click', () => {
+//     if (!header.classList.contains("is-apper") && !header.classList.contains("is-bottom") && !header.classList.contains("is-active")) {
+//         header.classList.add("is-appear");
+//         btn.classList.remove("is-close");
+//         btn.classList.add("is-open");
+//     }
+// })
+
+btn.addEventListener("click", () => {
+    if (btn.classList.contains("is-close")) {
+        btn.classList.remove("is-close");
+        btn.classList.add("is-open");
+        header.classList.add("is-appear");
+    } else {
+        btn.classList.remove("is-open");
+        btn.classList.add("is-close");
+        header.classList.remove("is-appear");
+    }
+}
+);
---
+++ JavaScript/js-slide.js
@@ -0,0 +1,71 @@
+const img = document.querySelector(".js-img");
+const bar = document.querySelectorAll(".js-bar");
+const prevBtn = document.querySelector(".js-prevBtn");
+const stopBtn = document.querySelector(".js-stopBtn");
+const stopImg = document.querySelector(".js-stopImg");
+const nextBtn = document.querySelector(".js-nextBtn");
+const title = document.querySelector(".js-title");
+const images = ["./images/baritone.JPG", "./images/sky.jpg", "./images/fujiQ.jpeg", "./images/TRPG.jpeg"];
+const texts = ["吹奏楽", "スキー", "遊園地", "TRPG"];
+let timer;
+
+let count = 0;
+let time = 10000;
+
+stopBtn.addEventListener("click", () => {
+    stopBtn.classList.toggle("is-stop");
+    if (stopBtn.classList.contains("is-stop")) {
+        clearInterval(timer);
+        stopImg.src = "./images/Play.png";
+    } else {
+        timer = setInterval(slide, time);
+        stopImg.src = "./images/Pause.png";
+    }
+}
+);
+
+prevBtn.addEventListener("click", () => {
+    if (count <= 0) {
+        count = images.length - 1;
+    } else {
+        count--;
+    }
+    img.src = images[count];
+    title.innerHTML = texts[count];
+    bar.forEach((item) => {
+        item.classList.remove("is-active");
+    });
+    bar[count].classList.add("is-active");
+}
+);
+
+nextBtn.addEventListener("click", () => {
+    if (count >= images.length - 1) {
+        count = 0;
+    } else {
+        count++;
+    }
+    img.src = images[count];
+    title.innerHTML = texts[count];
+    bar.forEach((item) => {
+        item.classList.remove("is-active");
+    });
+    bar[count].classList.add("is-active");
+}
+);
+
+const slide = () => {
+    if (count >= images.length - 1) {
+        count = 0;
+    } else {
+        count++;
+    }
+    img.src = images[count];
+    title.innerHTML = texts[count];
+    bar.forEach((item) => {
+        item.classList.remove("is-active");
+    });
+    bar[count].classList.add("is-active");
+}
+
+timer = setInterval(slide, time);
